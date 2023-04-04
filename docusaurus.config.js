0ad423ee7d9b9956ca46a3abd2db80dfcc6ebdfe
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2023-04-04 21:30:33 +0200

    Update docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Kadras',
-  tagline: 'Cloud Native Platform Toolkit',
+  tagline: 'Cloud Native Platforms',
   url: 'https://kadras-io.github.io',
   baseUrl: '/kadras-docs/',
   onBrokenLinks: 'throw',
